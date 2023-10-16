b332fb0133b1c40f0c628c0418dda53ab123d83b
Este <125880105+esteRossouw@users.noreply.github.com>
2023-10-16 13:19:56 +0200

    Task 3
    HTML Form Validation

--- scripts/main.js
+++ scripts/main.js
@@ -22,3 +22,21 @@ $(document).ready(function() {
         $(this).addClass('was-validated');
     });
 });
+document.getElementById('signupForm').addEventListener('submit', function (event) {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+
+    const regexNumber = /\d/;
+    const regexUpperCase = /[A-Z]/;
+    const regexLowerCase = /[a-z]/;
+
+    if (!regexNumber.test(password) || !regexUpperCase.test(password) || !regexLowerCase.test(password)) {
+        event.preventDefault();  
+        alert('Password must contain at least one number, one uppercase letter, and one lowercase letter.');
+    }
+
+    if (password !== confirmPassword) {
+        event.preventDefault(); 
+        alert('Passwords do not match.');
+    }
+});
